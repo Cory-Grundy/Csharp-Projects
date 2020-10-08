52a17032e7bb7ec572c22e69bb2df14a75a003a7
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2020-10-07 17:53:47 -0700

    Uploading new files

---
+++ C# .Net Framework/CSharpAssignment2.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAssignment2
+{
+    class Program
+    {
+        static void Main(Math[] args)
+        {
+            Console.WriteLine("What would you like to multiply by 50 today?");
+            int32 num1 = Console.ReadLine();
+            int32 product = num11 * 50;
+            Console.WriteLine(product);
+
+        }
+    }
+}
